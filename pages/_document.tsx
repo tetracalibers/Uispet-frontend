eec805d98a9b818a4d7908e59c860a4cd968bc1c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-23 12:45:26 +0900

    google fontsの設定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 // デフォルトのhtml, head, bodyタグを上書き
@@ -32,5 +39,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Hina+Mincho&display=swap'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Space+Mono:ital@0;1&display=swap'
+            rel='stylesheet'
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
