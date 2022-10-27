2a69792beca2781a990c9b5806239b3306833183
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-10-27 11:43:59 +0900

    [XyInputDemoPage] 座標計算と座標更新処理を実装

--- pages/demo/xyinput.tsx
+++ pages/demo/xyinput.tsx
@@ -1,10 +1,23 @@
 import { NextPage } from 'next'
+import { useCallback, useState } from 'react'
 import { XyInput } from '../../components/control/XyInput'
+import { IndicatorDragEvent } from '../../types/Event'
+import { getAreaXyCoords } from '../../utils/coords'
 
 type Props = {}
 
 const XyInputDemoPage: NextPage<Props> = () => {
-  return <XyInput coords={{ x: 0, y: 0 }} onChange={() => {}} />
+  const [xy, setXy] = useState({ x: 0, y: 0 })
+
+  const onMove = useCallback((e: IndicatorDragEvent) => {
+    const { x, y, width, height } = getAreaXyCoords(e)
+    setXy({
+      x: Math.round((x / width) * 100),
+      y: Math.round((y / height) * 100),
+    })
+  }, [])
+
+  return <XyInput coords={xy} onChange={onMove} />
 }
 
 export default XyInputDemoPage
